51d1c5372c37fd37ad0355977801ffc4ee43bee7
V1TSK <vitaly@easycoding.org>
2013-12-11 22:18:14 +0600

    Added launcher.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+ * Simple Source engine widget.
+ * 
+ * Copyright 2013 EasyCoding Team (ECTeam).
+ * Copyright 2005 - 2014 EasyCoding Team.
+ * 
+ * License: GNU GPL version 3.
+ *
+ * EasyCoding Team's official blog: http://www.easycoding.org/
+ * Project's official webpage: http://www.easycoding.org/projects/srvwidget
+ * 
+ */
+
+// Connecting libraries...
+require_once('libs/sourcequery/SourceQuery.class.php');
+require_once('libs/smarty/Smarty.class.php');
+require_once('libs/srvwidget/SrvWidget.class.php');
+
+// Starting application...
+Application::Run();
+
+?>
