ac7e6149e5b41389c0d341e10198e2c42dea6174
V1TSK <vitaly@easycoding.org>
2015-02-21 18:45:11 +0500

    Added function http_parse_headers. Taken from pecl_http package with our modifications.

--- libs/srvwidget/SrvWidget.class.php
+++ libs/srvwidget/SrvWidget.class.php
@@ -26,6 +26,21 @@ class Application
 	
 	private static $SERVERS = array();
 	
+	private function http_parse_headers($header)
+	{
+		$result = array();
+		$fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header));
+		foreach($fields as $field)
+		{
+			if (preg_match('/([^:]+): (.+)/m', $field, $match))
+			{
+				$match[1] = preg_replace('/(?<=^|[\x09\x20\x2D])./e', 'strtoupper("\0")', strtolower(trim($match[1])));
+				$result[$match[1]] = isset($result[$match[1]]) ? array($result[$match[1]], $match[2]) : trim($match[2]);
+			}
+		}
+		return $result;
+	}
+	
 	private function sendGETRequest($url, $useragent = 'wget')
 	{
 		$ch = curl_init();
