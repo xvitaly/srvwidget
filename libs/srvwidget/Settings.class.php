1b02dafce9f435b247c114d8d78a01c0158adcff
V1TSK <vitaly@easycoding.org>
2015-08-12 22:02:16 +0500

    Added GPL copyright.

--- libs/srvwidget/Settings.class.php
+++ libs/srvwidget/Settings.class.php
@@ -1,5 +1,18 @@
 <?php
 
+/* 
+ * Settings class. Part of Simple Source engine widget.
+ * 
+ * Copyright 2013 - 2015 EasyCoding Team (ECTeam).
+ * Copyright 2005 - 2015 EasyCoding Team.
+ * 
+ * License: GNU GPL version 3.
+ *
+ * EasyCoding Team's official blog: http://www.easycoding.org/
+ * Project's official webpage: http://www.easycoding.org/projects/srvwidget
+ * 
+ */
+
 class Settings
 {
 	/*
