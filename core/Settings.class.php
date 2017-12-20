4f6aa7be100aba32921d8d9499967dffcec55827
Vitaly Zaitsev <vitaly@easycoding.org>
2017-12-20 17:17:21 +0100

    Replaced tabs with spaces in Settings.class.php.

--- core/Settings.class.php
+++ core/Settings.class.php
@@ -17,25 +17,25 @@ namespace SrvWidget;
 
 class Settings
 {
-	/*
-	 * Steam settings.
-	 * Get your API key here: http://steamcommunity.com/dev/apikey
-	 */
-	const STEAM_URI = 'https://api.steampowered.com/IGameServersService/GetServerIPsBySteamID/v0001/';
-	const STEAM_TOKEN = '';
-	
-	/*
-	 * MySQL database settings.
-	 */
-	const DB_HOST = 'localhost';
-	const DB_NAME = 'srvwidget';
-	const DB_USER = '';
-	const DB_PASS = '';
-	
-	/*
-	 * Project settings.
-	 */
-	const LOCALE = 'ru_RU';
-	const SHOWLEGACY = false;
-	const RANDOMIZE = true;
+    /*
+     * Steam settings.
+     * Get your API key here: http://steamcommunity.com/dev/apikey
+     */
+    const STEAM_URI = 'https://api.steampowered.com/IGameServersService/GetServerIPsBySteamID/v0001/';
+    const STEAM_TOKEN = '';
+
+    /*
+     * MySQL database settings.
+     */
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'srvwidget';
+    const DB_USER = '';
+    const DB_PASS = '';
+
+    /*
+     * Project settings.
+     */
+    const LOCALE = 'ru_RU';
+    const SHOWLEGACY = false;
+    const RANDOMIZE = true;
 }
