3ad8030abb64c0ce347b20ed62486ef80cd26261
Vitaly Zaitsev <vitaly@easycoding.org>
2017-12-20 17:16:45 +0100

    Replaced tabs with spaces in Cache.class.php.

--- core/Cache.class.php
+++ core/Cache.class.php
@@ -16,38 +16,37 @@ namespace SrvWidget;
 
 class Cache
 {
-	const PAGE_TIME = 600;
-	
-	private $cacheFile;
-	
-	public function __construct()
-	{
-		$this -> cacheFile = sprintf('%s/%s.dat', sys_get_temp_dir(), hash('md5', $_SERVER['REQUEST_URI']));
-	}
-	
-	public function start()
-	{
-		if (file_exists($this -> cacheFile))
-		{
-			if ((time() - filemtime($this -> cacheFile)) < self::PAGE_TIME)
-			{
-				echo file_get_contents($this -> cacheFile);
-				exit();
-			}
-			else
-			{
-				unlink($this -> cacheFile);
-			}
-		}
-		ob_start();
-	}
-	
-	public function end()
-	{
-		$buf = str_replace(array("\t", "\n", "\r", "\r\n"), "", ob_get_contents());
-		file_put_contents($this -> cacheFile, $buf);
-		ob_end_clean();
-		echo $buf;
-	}
+    const PAGE_TIME = 600;
+    private $cacheFile;
+
+    public function __construct()
+    {
+        $this -> cacheFile = sprintf('%s/%s.dat', sys_get_temp_dir(), hash('md5', $_SERVER['REQUEST_URI']));
+    }
+
+    public function start()
+    {
+        if (file_exists($this -> cacheFile))
+        {
+            if ((time() - filemtime($this -> cacheFile)) < self::PAGE_TIME)
+            {
+                echo file_get_contents($this -> cacheFile);
+                exit();
+            }
+            else
+            {
+                unlink($this -> cacheFile);
+            }
+        }
+        ob_start();
+    }
+
+    public function end()
+    {
+        $buf = str_replace(array("\t", "\n", "\r", "\r\n"), "", ob_get_contents());
+        file_put_contents($this -> cacheFile, $buf);
+        ob_end_clean();
+        echo $buf;
+    }
 }
 ?>
